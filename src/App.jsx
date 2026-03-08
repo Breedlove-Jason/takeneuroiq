1acf0899ff2ca963f4759720cb75555dcec57361
Jason Breedlove <j@jasonbreedlove.com>
2026-03-07 22:02:59 -0800

    enhance App component styling with Tailwind CSS and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="flex justify-center space-x-4">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,9 +16,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className="text-5xl font-bold my-8">Vite + React</h1>
+      <div className="card bg-gray-800 p-8 rounded-xl shadow-lg mb-4">
+        <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-200" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
